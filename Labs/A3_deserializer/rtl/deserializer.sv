fff36976ca5acd4b3abade177ebd5064eab59e8c
GorosVi <gorosvia@gmail.com>
2017-01-29 21:30:56 +0300

    Added solution for task A3_deserializer

--- Labs/A3_deserializer/rtl/deserializer.sv
+++ Labs/A3_deserializer/rtl/deserializer.sv
@@ -11,5 +11,41 @@ module deserializer #(
 	output logic             deser_data_val_o
 );
 
+// Bits are counted in range [1..2**WIDTH], and standard
+//  width [$clog2(WIDTH):0] causes overflow situation
+logic [$clog2(WIDTH)+1:0] rx_bit_count;
+logic [WIDTH-1:0]         rx_data;
+
+
+always_ff @(posedge clk_i)
+	begin
+		deser_data_val_o <= 0;
+		if( srst_i )
+			begin
+				rx_bit_count <= 0;
+				rx_data <= 0;
+				deser_data_o <= 0;
+			end
+		else
+			begin
+				if( data_val_i == 1'b1 )
+					begin
+						rx_bit_count <= rx_bit_count + 1'b1;
+						rx_data <= {rx_data, data_i};
+					end
+				// Receiving complete
+				if( rx_bit_count == WIDTH )
+					begin
+						deser_data_val_o <= 1'b1;
+						deser_data_o <= rx_data;
+						// Setting rx_bit_count to 1 when receiving is active -
+						//  current bit will be received in this clock cycle
+						if( data_val_i == 1'b1 )
+							rx_bit_count <= 1;
+						else
+							rx_bit_count <= 0;
+					end
+			end
+	end
 
 endmodule
--- Labs/A3_deserializer/tb/tb.sv
+++ Labs/A3_deserializer/tb/tb.sv
@@ -18,8 +18,8 @@ logic             deser_data_val_o;
 deserializer #(
 	.WIDTH             ( WIDTH             )
 ) i_deserializer (
-	.clk_i             ( clk_i             ),
-	.srst_i            ( srst_i            ),
+	.clk_i             ( clk               ),
+	.srst_i            ( srst              ),
 
 	.data_i            ( data_i            ),
 	.data_val_i        ( data_val_i        ),
@@ -43,9 +43,80 @@ initial
 	end
 
 
+logic [WIDTH-1:0] data_temp_storg;
+
 initial
 	begin : test_sequence_generator
-		repeat (10) @( posedge clk );
+		data_i     = 1'b0;
+		data_val_i = 1'b0;
+		repeat (3) @( posedge clk );
+
+		// TEST 1: run WIDTH clock cycles with data_val_i active. Module should give result to output
+		data_val_i = 1'b1;
+		data_i     = 1'b1;
+		repeat (WIDTH)
+			begin
+				@( posedge clk );
+				data_i     = ~data_i;
+				assert ( deser_data_val_o == 0 ) else $error ("Error: deser_data_o is ready too fast (test 1_1)");
+			end
+		data_val_i = 1'b0;
+		data_i     = 1'b0;
+		@( posedge clk );
+		@( posedge clk );
+		assert ( deser_data_val_o == 1 ) else $error ("Error: deser_data_o not ready (test 1_2)");
+
+		// TEST 2: run WIDTH*2 clock cycles with data_val_i active in half of this. Module should give result to output.
+		repeat (WIDTH)
+			begin
+				data_i     = 1'b1;
+				data_val_i = 1'b1;
+				@( posedge clk );
+				assert ( deser_data_val_o == 0 ) else $error ("Error: deser_data_o is ready too fast (test 2_1)");
+				data_i     = 1'b0;
+				data_val_i = 1'b0;
+				@( posedge clk );
+				assert ( deser_data_val_o == 0 ) else $error ("Error: deser_data_o is ready too fast (test 2_2)");
+			end
+		@( posedge clk );
+		assert ( deser_data_val_o == 1 ) else $error("Error: deser_data_o not ready (test 2_3)");
+
+		// TEST 3 : correctly continuous run. Comparing results from two sequential (non-stop)
+		//  runs with identical data pattern.
+		data_val_i = 1'b1;
+		data_i     = 1'b1;
+		repeat (WIDTH)
+			begin
+				@( posedge clk );
+				data_i = ~data_i;
+			end
+		// Invert data for odd deserializer widths, otherwise results of two sequential runs will be not same
+		if( ( WIDTH % 2 ) > 0 )
+			data_i = ~data_i;
+
+		@( posedge clk );
+		data_i = ~data_i;
+		@( posedge clk );
+		data_i = ~data_i;
+
+		assert ( deser_data_val_o == 1 ) else $error("Error: deser_data_o not ready (test 3_1)");
+		data_temp_storg = deser_data_o;
+
+		repeat (WIDTH - 2)
+			begin
+				@( posedge clk );
+				data_i = ~data_i;
+			end
+
+		data_val_i = 1'b0;
+		data_i     = 1'b0;
+		@( posedge clk );
+		@( posedge clk );
+		assert ( deser_data_val_o == 1 ) else $error ("Error: deser_data_o not ready (test 3_2)");
+		assert ( data_temp_storg == deser_data_o ) else $error ("Error: deser_data_o not matched with previous result (test 3_3)");
+
+		data_val_i = 1'b0;
+		repeat (5) @( posedge clk );
 		$stop;
 	end
 
