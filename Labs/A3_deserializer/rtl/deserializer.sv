899955de816ffc5aa65ff2b3a8a885363dc58672
GorosVi <gorosvia@gmail.com>
2017-02-06 21:41:14 +0300

    Added fixed solution for task A3_deserializer

--- Labs/A3_deserializer/rtl/deserializer.sv
+++ Labs/A3_deserializer/rtl/deserializer.sv
@@ -18,33 +18,39 @@ logic [WIDTH-1:0]         rx_data;
 
 
 always_ff @(posedge clk_i)
-	begin
-		deser_data_val_o <= 0;
+	begin : receiving_data
 		if( srst_i )
 			begin
+				// rx_data      <= 0;
 				rx_bit_count <= 0;
-				rx_data <= 0;
-				deser_data_o <= 0;
 			end
 		else
 			begin
 				if( data_val_i == 1'b1 )
-					begin
-						rx_bit_count <= rx_bit_count + 1'b1;
-						rx_data <= {rx_data, data_i};
-					end
+					rx_data <= {rx_data, data_i};
+
 				// Receiving complete
-				if( rx_bit_count == WIDTH )
-					begin
-						deser_data_val_o <= 1'b1;
-						deser_data_o <= rx_data;
-						// Setting rx_bit_count to 1 when receiving is active -
-						//  current bit will be received in this clock cycle
-						if( data_val_i == 1'b1 )
-							rx_bit_count <= 1;
-						else
-							rx_bit_count <= 0;
-					end
+				if( rx_bit_count < WIDTH )
+					rx_bit_count <= ( data_val_i == 1'b1 ) ? ( rx_bit_count + 1'b1 ) :
+					                                         ( rx_bit_count        );
+				else // When receiving is active - write value considering received bit at this clock cycle
+					rx_bit_count <= ( data_val_i == 1'b1 ) ? ( 1'b1                ) :
+					                                         ( 1'b0                );
+			end
+	end
+
+
+always_ff @(posedge clk_i)
+	begin : output_data_when_receiving_completed
+		if( rx_bit_count == WIDTH )
+			begin
+				deser_data_o     <= rx_data;
+				deser_data_val_o <= 1'b1;
+			end
+		else
+			begin
+				// deser_data_o     <= 0;
+				deser_data_val_o <= 0;
 			end
 	end
 
