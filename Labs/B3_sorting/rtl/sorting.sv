7e6bdd17eda1b17dd1b4bf3c7566dc9538b2eef9
GorosVi <gorosvia@gmail.com>
2017-02-07 19:19:48 +0300

    Added working variant of B3 task, but synthesized without RAM

--- Labs/B3_sorting/rtl/sorting.sv
+++ Labs/B3_sorting/rtl/sorting.sv
@@ -18,9 +18,6 @@ module sorting #(
 	output logic              busy_o
 );
 
-logic sorting_ok;
-
-
 localparam ARRAY_SIZE = ( 2 ** AWIDTH - 1 );
 
 reg   [DWIDTH-1:0] mem [ARRAY_SIZE:0];
@@ -28,6 +25,11 @@ reg   [DWIDTH-1:0] mem [ARRAY_SIZE:0];
 enum logic [1:0] { RECEIVE_S,
                    SORTING_S,
                    OUTPUT_S } state, state_next;
+
+logic[AWIDTH-1:0] out_read_ptr;
+logic[DWIDTH-1:0] out_read_data;
+
+logic sorting_ok;
 
 
 always_ff @( posedge clk_i )
@@ -47,36 +49,24 @@ always_comb
 		state_next = state;
 
 		case( state )
+
 			RECEIVE_S :
-				begin
-					if( eop_i )
-						begin
-							state_next = SORTING_S;
-						end
-				end
+				if( eop_i )
+					state_next = SORTING_S;
+
 			SORTING_S :
-				begin
-					if( sorting_ok )
-						begin
-							state_next = OUTPUT_S;
-						end
-				end
+				if( sorting_ok )
+					state_next = OUTPUT_S;
+
 			OUTPUT_S :
-				begin
-					if( eop_o )
-						begin
-							state_next = RECEIVE_S;
-						end
-				end
+				if( eop_o )
+					state_next = RECEIVE_S;
 
 			default :
-				begin
 					state_next = RECEIVE_S;
-				end
+
 		endcase
 	end
-
-
 
 
 logic[AWIDTH-1:0] input_words_count;
@@ -104,48 +94,29 @@ always_ff @( posedge clk_i )
 assign input_words_count = write_ptr;
 
 
-
-
-logic[AWIDTH-1:0] max_read_ptr,
-                  min_read_ptr,
-                  max_sorted_addr,
-                  min_sorted_addr;
-
+logic[AWIDTH-1:0] min_read_ptr,
+                  max_read_ptr;
 logic[DWIDTH-1:0] min_read_data,
-                  max_read_data,
-                  min_write_data,
-                  max_write_data;
-
+                  max_read_data;
 logic data_swap;
 
 always_ff @( posedge clk_i )
 	begin
 		if( state == SORTING_S )
 			begin
-				if( min_read_ptr == min_sorted_addr )
-					min_read_ptr  <= min_read_ptr + 1'b1;
-				else if( min_read_ptr == max_sorted_addr)
+				if( min_read_ptr == max_read_ptr - 1'b1 )
 					begin
-						max_sorted_addr <= max_sorted_addr - 1;
-						max_read_ptr <= max_read_ptr - 1;
 						min_read_ptr <= 0;
+						max_read_ptr <= max_read_ptr - 1;
 					end
 				else
-					min_read_ptr  <= min_read_ptr + 1'b1;
+					min_read_ptr <= min_read_ptr + 1'b1;
 			end
 		else
 			begin
-				max_sorted_addr <= write_ptr_new;// input_words_count;
-				max_read_ptr <= write_ptr_new;//input_words_count;
-				min_sorted_addr <= '0;
 				min_read_ptr <= '0;
+				max_read_ptr <= write_ptr_new; //input_words_count;
 			end
-
-		// if( state == SORTING_S )
-		// 	sorting_ok <= ( min_read_ptr == max_sorted_addr ) ? ( 1 ) : ( 0 );
-		// else
-			sorting_ok <= 0;
-
 	end
 
 always_comb
@@ -154,24 +125,29 @@ always_comb
 			data_swap <= 1'b1;
 		else
 			data_swap <= 0;
+
+		if( state == SORTING_S )
+			sorting_ok <= ( max_read_ptr == 0 );
+		else
+			sorting_ok <= 0;
 	end
 
 
+logic [AWIDTH-1:0] mem_ptr;
 
-assign busy_o = data_swap;
-assign data_o = min_read_data;
 always_comb
 	begin : memory_read_operations
+		mem_ptr = 'x;
+		min_read_data = 'x;
+		max_read_data = 'x;
+		out_read_data = 'x;
 		if( state == SORTING_S )
 			begin
 				min_read_data = mem[min_read_ptr];
 				max_read_data = mem[max_read_ptr];
 			end
-		else
-			begin
-				min_read_data = 'x;
-				max_read_data = 'x;
-			end
+		else if( state == OUTPUT_S )
+			out_read_data = mem[out_read_ptr];
 	end
 
 always_ff @( posedge clk_i )
@@ -186,42 +162,43 @@ always_ff @( posedge clk_i )
 	end
 
 
-
-logic[AWIDTH-1:0] read_ptr;
-
 always_ff @( posedge clk_i )
 	begin
 		if( srst_i )
-			read_ptr <= 0;
+			out_read_ptr <= 0;
 		else
 			if( state == OUTPUT_S )
-				if( read_ptr <= input_words_count )
+				if( out_read_ptr <= input_words_count )
 					begin
-						read_ptr <= read_ptr + 1'b1;
-						//data_o <= mem[read_ptr];
+						out_read_ptr <= out_read_ptr + 1'b1;
+						data_o <= out_read_data;
 					end
 				else
-					read_ptr <= 0;
+					out_read_ptr <= 0;
 	end
 
 always_ff @( posedge clk_i )
 	begin
-		if( ( state == OUTPUT_S ) && ( read_ptr == 1'b0 ) )
+		if( ( state_next == OUTPUT_S ) && ( out_read_ptr == 1'b0 ) )
 			sop_o <= 1'b1;
 		else
 			sop_o <= 0;
 
-		if( ( state == OUTPUT_S ) && ( read_ptr == input_words_count ) )
+		if( ( state_next == OUTPUT_S ) && ( out_read_ptr == input_words_count ) )
 			eop_o <= 1'b1;
 		else
 			eop_o <= 0;
 
-		if( ( state == OUTPUT_S ) && ( read_ptr <= input_words_count ) )
+		if( ( state_next == OUTPUT_S ) && ( out_read_ptr <= input_words_count ) )
 			val_o <= 1'b1;
 		else
 			val_o <= 0;
-	end
 
+		if( ( state_next == SORTING_S ) || ( state_next == OUTPUT_S ) && ( out_read_ptr <= input_words_count ) )
+			busy_o <= 1'b1;
+		else
+			busy_o <= 0;
+	end
 
 
 endmodule
